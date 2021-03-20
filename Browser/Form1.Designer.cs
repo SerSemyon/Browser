ab99415fae741e13b4f546897caeae925ed89581
YtkaYtkaGagaga <sersemyon@gmail.com>
2021-03-20 19:21:35 +0300

    добавлены закладки

--- Browser/Form1.Designer.cs
+++ Browser/Form1.Designer.cs
@@ -29,17 +29,18 @@ namespace Browser
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(browserForm));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.refreshButton = new System.Windows.Forms.Button();
-            this.backButton = new System.Windows.Forms.Button();
-            this.nextButton = new System.Windows.Forms.Button();
-            this.addButton = new System.Windows.Forms.Button();
-            this.deleteButton = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.bookmarksButton = new System.Windows.Forms.Button();
+            this.addBookmarkButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -82,8 +83,8 @@ namespace Browser
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel2.Controls.Add(this.button8, 8, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button7, 7, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button6, 6, 0);
+            this.tableLayoutPanel2.Controls.Add(this.bookmarksButton, 7, 0);
+            this.tableLayoutPanel2.Controls.Add(this.addBookmarkButton, 6, 0);
             this.tableLayoutPanel2.Controls.Add(this.deleteButton, 5, 0);
             this.tableLayoutPanel2.Controls.Add(this.addButton, 4, 0);
             this.tableLayoutPanel2.Controls.Add(this.nextButton, 1, 0);
@@ -98,49 +99,37 @@ namespace Browser
             this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 34);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
-            // refreshButton
+            // button8
             // 
-            this.refreshButton.BackgroundImage = global::Browser.Properties.Resources.refresh;
-            this.refreshButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.refreshButton.Location = new System.Drawing.Point(83, 3);
-            this.refreshButton.Name = "refreshButton";
-            this.refreshButton.Size = new System.Drawing.Size(34, 28);
-            this.refreshButton.TabIndex = 0;
-            this.refreshButton.UseVisualStyleBackColor = true;
-            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            this.button8.BackgroundImage = global::Browser.Properties.Resources.history;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button8.Location = new System.Drawing.Point(757, 3);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(34, 28);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
             // 
-            // backButton
+            // bookmarksButton
             // 
-            this.backButton.BackgroundImage = global::Browser.Properties.Resources.back;
-            this.backButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.backButton.Location = new System.Drawing.Point(3, 3);
-            this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(34, 28);
-            this.backButton.TabIndex = 1;
-            this.backButton.UseVisualStyleBackColor = true;
-            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            this.bookmarksButton.BackgroundImage = global::Browser.Properties.Resources.bookmark;
+            this.bookmarksButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bookmarksButton.Location = new System.Drawing.Point(717, 3);
+            this.bookmarksButton.Name = "bookmarksButton";
+            this.bookmarksButton.Size = new System.Drawing.Size(34, 28);
+            this.bookmarksButton.TabIndex = 7;
+            this.bookmarksButton.UseVisualStyleBackColor = true;
+            this.bookmarksButton.Click += new System.EventHandler(this.bookmarksButton_Click);
             // 
-            // nextButton
+            // addBookmarkButton
             // 
-            this.nextButton.BackgroundImage = global::Browser.Properties.Resources.forward;
-            this.nextButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.nextButton.Location = new System.Drawing.Point(43, 3);
-            this.nextButton.Name = "nextButton";
-            this.nextButton.Size = new System.Drawing.Size(34, 28);
-            this.nextButton.TabIndex = 2;
-            this.nextButton.UseVisualStyleBackColor = true;
-            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
-            // 
-            // addButton
-            // 
-            this.addButton.BackgroundImage = global::Browser.Properties.Resources.add;
-            this.addButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.addButton.Location = new System.Drawing.Point(597, 3);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(34, 28);
-            this.addButton.TabIndex = 4;
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            this.addBookmarkButton.BackgroundImage = global::Browser.Properties.Resources.addBookmark;
+            this.addBookmarkButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.addBookmarkButton.Location = new System.Drawing.Point(677, 3);
+            this.addBookmarkButton.Name = "addBookmarkButton";
+            this.addBookmarkButton.Size = new System.Drawing.Size(34, 28);
+            this.addBookmarkButton.TabIndex = 6;
+            this.addBookmarkButton.UseVisualStyleBackColor = true;
+            this.addBookmarkButton.Click += new System.EventHandler(this.addBookmarkButton_Click);
             // 
             // deleteButton
             // 
@@ -153,35 +142,49 @@ namespace Browser
             this.deleteButton.UseVisualStyleBackColor = true;
             this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
             // 
-            // button6
+            // addButton
             // 
-            this.button6.BackgroundImage = global::Browser.Properties.Resources.addBookmark;
-            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button6.Location = new System.Drawing.Point(677, 3);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(34, 28);
-            this.button6.TabIndex = 6;
-            this.button6.UseVisualStyleBackColor = true;
+            this.addButton.BackgroundImage = global::Browser.Properties.Resources.add;
+            this.addButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.addButton.Location = new System.Drawing.Point(597, 3);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(34, 28);
+            this.addButton.TabIndex = 4;
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
-            // button7
+            // nextButton
             // 
-            this.button7.BackgroundImage = global::Browser.Properties.Resources.bookmark;
-            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button7.Location = new System.Drawing.Point(717, 3);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(34, 28);
-            this.button7.TabIndex = 7;
-            this.button7.UseVisualStyleBackColor = true;
+            this.nextButton.BackgroundImage = global::Browser.Properties.Resources.forward;
+            this.nextButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.nextButton.Location = new System.Drawing.Point(43, 3);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(34, 28);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
-            // button8
+            // backButton
             // 
-            this.button8.BackgroundImage = global::Browser.Properties.Resources.history;
-            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button8.Location = new System.Drawing.Point(757, 3);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(34, 28);
-            this.button8.TabIndex = 8;
-            this.button8.UseVisualStyleBackColor = true;
+            this.backButton.BackgroundImage = global::Browser.Properties.Resources.back;
+            this.backButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.backButton.Location = new System.Drawing.Point(3, 3);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(34, 28);
+            this.backButton.TabIndex = 1;
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.BackgroundImage = global::Browser.Properties.Resources.refresh;
+            this.refreshButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.refreshButton.Location = new System.Drawing.Point(83, 3);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(34, 28);
+            this.refreshButton.TabIndex = 0;
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
             // richTextBox1
             // 
@@ -195,7 +198,9 @@ namespace Browser
             this.richTextBox1.TabIndex = 9;
             this.richTextBox1.Text = "";
             this.richTextBox1.WordWrap = false;
+            this.richTextBox1.Enter += new System.EventHandler(this.richTextBox1_Enter);
             this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            this.richTextBox1.Leave += new System.EventHandler(this.richTextBox1_Leave);
             // 
             // browserForm
             // 
@@ -203,8 +208,10 @@ namespace Browser
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "browserForm";
             this.Text = "Браузер";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.browserForm_FormClosing);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -218,8 +225,8 @@ namespace Browser
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button bookmarksButton;
+        private System.Windows.Forms.Button addBookmarkButton;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button nextButton;
--- Browser/Form1.cs
+++ Browser/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Browser
 {
@@ -14,14 +15,83 @@ namespace Browser
     {
         int i = 0; //индекс вкладки
         bool isPageCompleted = false;
+        List<bookmark> bookmarks;
+        FileStream streamHtml;
+        void ReadBookmarks()
+        {
+            StreamReader reader = new StreamReader("Bookmarks.txt");
+            string fileString = reader.ReadToEnd();
+            string[] fileData = fileString.Split('\n');
+            bookmarks = new List<bookmark>(fileData.Length/2);
+            for (int i = 0; i < fileData.Length - 2; i += 2)
+            {
+                bookmarks.Add(new bookmark(fileData[i], fileData[i + 1]));
+            }
+            reader.Close();
+        }
+        void SaveBookmarks()
+        {
+            StreamWriter writer = new StreamWriter("Bookmarks.txt");
+            foreach (bookmark book in bookmarks)
+            {
+                writer.Write(book.name+ '\n'+book.url+'\n');
+            }
+            writer.Close();
+        }
+
+        private void HtmlDocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            streamHtml.Close();
+            tabControl1.SelectedTab.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+        }
+        void CreateHtmlBookmarks()
+        {
+            StreamWriter streamwriter = new StreamWriter("lastBookmarks.html");
+            streamwriter.WriteLine("<html>");
+            streamwriter.WriteLine("<head>");
+            streamwriter.WriteLine("  <title>Закладки</title>");
+            streamwriter.WriteLine("  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
+            streamwriter.WriteLine("</head>");
+            streamwriter.WriteLine("<body>");
+            foreach (bookmark mark in bookmarks)
+            {
+                streamwriter.WriteLine("<p><a href="+ mark.url+">"+mark.name+"</a></p>");
+            }
+            streamwriter.WriteLine("</body>");
+            streamwriter.WriteLine("</html>");
+            streamwriter.Close();
+        }
         public browserForm()
         {
             InitializeComponent();
             AddTab();
+            ReadBookmarks();
         }
         void AddTab()
         {
             AddTab("Yandex.ru");
+        }
+
+        void OpenHtml(string nameFile)
+        {
+            streamHtml = new FileStream(nameFile, FileMode.Open);
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentStream = streamHtml;
+        }
+        void AddHtmlTab(string nameFile)
+        {
+            WebBrowser web = new WebBrowser();
+            web.Visible = true; 
+            web.ScriptErrorsSuppressed = true; 
+            web.Dock = DockStyle.Fill; 
+            web.NewWindow += NewWindow;
+            web.Navigated += SaveInHistory;
+            tabControl1.TabPages.Add("New Pages"); 
+            tabControl1.SelectTab(i); 
+            tabControl1.SelectedTab.Controls.Add(web); 
+            i += 1; // вкладка с индексом один
+            web.DocumentCompleted += HtmlDocumentCompleted;
+            streamHtml = new FileStream(nameFile, FileMode.Open);
+            web.DocumentStream = streamHtml;
         }
         void AddTab(string url)
         {
@@ -31,12 +101,17 @@ namespace Browser
             web.Dock = DockStyle.Fill; // сделаем его на полный экран
             web.DocumentCompleted += DocumentCompleted;
             web.NewWindow += NewWindow;
-            
+            web.Navigated += SaveInHistory;
             tabControl1.TabPages.Add("New Pages"); // добавление влкадок
             tabControl1.SelectTab(i); // выделяет поределнную вкладку
             tabControl1.SelectedTab.Controls.Add(web); //то, что данной вкладой управляет
             i += 1; // вкладка с индексом один
             OpenUrl(url);
+        }
+
+        void SaveInHistory(object sender, WebBrowserNavigatedEventArgs e)
+        {
+
         }
         private void NewWindow(object sender, CancelEventArgs e)
         {
@@ -60,22 +135,36 @@ namespace Browser
         }
         private void refreshButton_Click(object sender, EventArgs e)
         {
-            if (isPageCompleted)
+            if (((WebBrowser)tabControl1.SelectedTab.Controls[0]).Url.ToString() != "about:blank")
             {
-                string newUrl = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Url.ToString();
-                OpenUrl(newUrl);
+                if (isPageCompleted)
+                {
+                    string newUrl = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Url.ToString();
+                    OpenUrl(newUrl);
+                }
+                else
+                {
+                    ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Stop();
+                    isPageCompleted = true;
+                    refreshButton.BackgroundImage = Properties.Resources.refresh;
+                }
             }
             else
             {
-                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Stop(); 
-                isPageCompleted = true;
-                refreshButton.BackgroundImage = Properties.Resources.refresh;
+                if (tabControl1.SelectedTab.Text == "Закладки")
+                {
+                    CreateHtmlBookmarks();
+                    OpenHtml("lastBookmarks.html");
+                }
+                else if (tabControl1.SelectedTab.Text == "История")
+                {
+
+                }
             }
         }
 
         private void backButton_Click(object sender, EventArgs e)
         {
-
             ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoBack();
         }
 
@@ -105,6 +194,44 @@ namespace Browser
             {
                 OpenUrl(richTextBox1.Text);
             }
+        }
+
+        private void browserForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveBookmarks();
+        }
+
+        private void addBookmarkButton_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab.Text != "" && tabControl1.SelectedTab.Text !="Закладки")
+            bookmarks.Add(new bookmark(tabControl1.SelectedTab.Text, ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Url.ToString()));
+        }
+
+        private void richTextBox1_Enter(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Url.ToString();
+        }
+
+        private void richTextBox1_Leave(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void bookmarksButton_Click(object sender, EventArgs e)
+        {
+            CreateHtmlBookmarks();
+            AddHtmlTab("lastBookmarks.html");
+        }
+    }
+
+    class bookmark
+    {
+        public string name;
+        public string url;
+        public bookmark(string newName, string newUrl)
+        {
+            name = newName;
+            url = newUrl;
         }
     }
 }
